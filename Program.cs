1197d73efb6e96a5c683af45aa9a4e9b04cb1416
逸舟 黄 <584041597@qq.com>
2025-11-24 20:53:00 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Playwright;
+
+namespace MicrosoftRewardAutomation
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                using var pw = await Playwright.CreateAsync();
+                await using var browser = await pw.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+                {
+                    Channel="msedge",
+                    Headless=false
+                });
+                var page = await browser.NewPageAsync();
+                await page.GotoAsync("https://www.bing.com");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("脚本出错辣，下面是出错内容。");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
